3aad2690a9458c7e5970b6497e23cb6079d80bdc
allin <aa704538319@163.com>
2018-09-18 07:53:31 +0800

    增加首页views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'blog/index.html', context={
+        'title': 'Blog title',
+        'welcome': 'Welcome!'
+    })
