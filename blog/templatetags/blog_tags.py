f017291432c658eb431a5c9aab787d438f8d9495
allin <aa704538319@163.com>
2018-09-18 16:40:04 +0800

    使用自定义模板标签去实时更新侧边栏(半成品)

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,21 @@
+from django import template
+from ..models import Post, Category
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_recent_posts(num=5):
+    """最新文章模板标签"""
+    return Post.objects.all().order_by('-created_time')[:num]
+
+
+@register.simple_tag
+def archives():
+    """归档模板标签"""
+    return Post.objects.dates('created_time', 'month', order='DESC')
+
+@register.simple_tag
+def get_categories():
+    """分类模板标签"""
+    return Category.objects.all()
