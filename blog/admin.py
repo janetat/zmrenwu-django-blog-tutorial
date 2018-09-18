129003a1a8cc9f8b1e1a65bdc032d860245126e1
allin <aa704538319@163.com>
2018-09-18 11:47:37 +0800

    定制后台 by admin.ModelAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Category, Tag
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
+
