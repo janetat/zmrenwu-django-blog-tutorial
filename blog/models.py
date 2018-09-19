d0dcfb9ac13fec2a3f79bbdabec423b41a268c4f
allin <aa704538319@163.com>
2018-09-19 15:01:53 +0800

    自动生成文章摘要(修改model的save方法)

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
+import markdown
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from django.utils.html import strip_tags
 
 # Create your models here.
 class Category(models.Model):
@@ -36,9 +37,27 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        """ 实现自动摘要
+            方法一：在被保存到数据库之前，从body自动摘取前50个字符，成为摘要abstract。
+            方法二：在模板中使用过滤器{{ post.body|truncatechars:54 }}，不过如果有html标签，会比较难看。
+        """
+        if not self.abstract:
+            # 实例化Markdown类，用于渲染body文本
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            # 将Markdown转换成HTML，然后去掉HTML文本的全部标签，从文本摘取前50个字符赋给abstract
+            self.abstract = strip_tags(md.convert(self.body))[:50]
+
+        super(Post, self).save(*args, **kwargs)
+
+
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
 
     def increase_page_view(self):
         self.page_view += 1
         self.save(update_fields=['page_view'])
+
