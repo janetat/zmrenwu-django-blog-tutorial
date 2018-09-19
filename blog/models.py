26ef002eefa38371721dfc0614e583d761cf087b
allin <aa704538319@163.com>
2018-09-19 16:35:39 +0800

    CBV，基于类的通用视图: ListView和DetailView

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.html import strip_tags
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -53,11 +54,9 @@ class Post(models.Model):
 
         super(Post, self).save(*args, **kwargs)
 
-
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
 
     def increase_page_view(self):
         self.page_view += 1
         self.save(update_fields=['page_view'])
-
--- blog/views.py
+++ blog/views.py
@@ -1,45 +1,68 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from comments.forms import CommentForm
 from .models import Post, Category
 
 
-# Create your views here.
-def index(request):
-    post_list = Post.objects.all()
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class IndexView(ListView):
+    # get_queryset()对Post进行操作
+    model = Post
+    template_name = 'blog/index.html'
+    # 获取Post列表数据，并保存到post_list中
+    context_object_name = 'post_list'
 
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.body = markdown.markdown(post.body, extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        'markdown.extensions.toc'
-    ])
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
 
-    # 阅读量+1
-    post.increase_page_view()
+    # get_object, get_context_data都是辅助方法，都会在get中调用
+    def get(self, request, *args, **kwargs):
+        """get方法最终会返回一个HTTPResponse"""
+        # 调用父类的get, 才有self.object
+        response = super().get(request, *args, **kwargs)
 
-    # 创建评论表单实例
-    form = CommentForm()
-    # 因为Comment和Post是多对一关系，是ForeignKey。模型Post有Comment的反向引用。
-    comment_list = post.comment_set.all()
-    context = {
-        'post': post,
-        'form': form,
-        'comment_list': comment_list,
-    }
-    return render(request, 'blog/detail.html', context=context)
+        # 文章阅读量+1。这里的object就是get_object()返回的object, 就是文章post的一个实例
+        self.object.increase_page_view()
+
+        return response
+
+    def get_object(self, queryset=None):
+        """在这里对post的body的值解析为Markdown"""
+        post = super().get_object(queryset=None)
+        post.body = markdown.markdown(post.body,
+                                      extensions=[
+                                          'markdown.extensions.extra',
+                                          'markdown.extensions.codehilite',
+                                          'markdown.extensions.toc'
+                                      ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        """传递除context_object_name以外的变量到模板"""
+        context = super().get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form': form,
+            'comment_list': comment_list,
+        })
+        return context
 
 
-def archives(request, year, month):
-    post_list = Post.objects.filter(created_time__year=year, created_time__month=month)
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class ArchivesView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super().get_queryset().filter(created_time__year=year, created_time__month=month)
 
 
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate)
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class CategoryView(IndexView):
+    def get_queryset(self):
+        # 类视图中，从URL捕获的命名组参数值保存在self.kwargs中，非命名组参数保存在args中。
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        # 因为父类IndexView绑定了model = Post, 所以get_queryset对Post操作
+        return super().get_queryset().filter(category=cate)
